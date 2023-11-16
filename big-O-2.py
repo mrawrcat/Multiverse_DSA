31b4c65322cdcd81099af79c5308664176a58d2f
danwen <danwen@penguin>
2023-11-16 15:43:55 -0500

    summarize big-O-2

---
+++ big-O-2.py
@@ -0,0 +1,9 @@
+# big-O-2
+
+'''contains-duplicate problem
+Summarize:
+We have an array that contains at least one element and at most 105 elements.
+(these elements are integers)
+We want to return true if there are any duplicates and false 
+if there are no duplicates. 
+'''
