d959263307711be1dd17f7981b432cbe1ff7e76d
danwen <danwen@penguin>
2023-11-16 16:30:46 -0500

    plan algorithm psuedocode

--- big-O-2.py
+++ big-O-2.py
@@ -6,6 +6,16 @@ We have an array that contains at least one element and at most 105 elements.
 (these elements are integers)
 We want to return true if there are any duplicates and false 
 if there are no duplicates. 
+
+Pseudocode:
+First declare the function with a parameter that accepts an array.
+Check if the array has only one element. If it does return false.
+(One element = no duplicates so every element is distinct)
+Create an empty hash map (dictionary) that stores the integer we iterate through in the array
+Every iteration check if this integer is stored in the hash map
+If there is a corresponding value then output true.
+If there is not add the number in the hash map.
+when we get to the end return false 
 '''
 
 
