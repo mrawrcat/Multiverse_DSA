21c2493b135532462146cbe1541020e4116fab0d
danwen <danwen@penguin>
2023-11-30 16:29:21 -0500

    added solution

--- ransomNote.py
+++ ransomNote.py
@@ -22,21 +22,36 @@ Psuedocode planning:
 
 To see if magazine has enough characters to spell out the string in ransomNote we need to be able to compare the characters
 in both strings.
-To do this we can first make both strings into lists.
+To do this we can first make both strings into lists. Then we can iterate through every character in ransomNote and check if the
+character is in magazine. If it is in magazine, remove that first character we find in magazine. If we complete the loop and checked
+all of the characters, we can safely say that magazine has the required characters to spell out the ransomNote.
 
-
+This solution is O(n) because we iterate through ransomNote once
 '''
 
-def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-    
-    '''
-    '''
-    
-ransomNote = ['a','b','c']
-magazine = ['b','a','c','d','a','b','c','a','b']
 
-for char in ransomNote:
-    if char in magazine:
-        magazine.remove(char)
-        print(magazine)
-            
+def canConstruct(ransomNote: str, magazine: str) -> bool:
+    """
+    :type ransomNote: str
+    :type magazine: str
+    :rtype: bool
+    """
+    ransomNote = list(ransomNote)
+    magazine = list(magazine)
+    for char in ransomNote:
+        if char in magazine:
+            magazine.remove(char)
+        else:
+            return False
+    return True
+
+
+note = 'Abraca dabra'
+
+magazine = 'cadabraabra lsv' #no uppercase
+magazine2 = 'cadabraabrAlsv' #no space
+magazine3 = 'cadabr aabrAlsv' #space and uppercase
+
+print(canConstruct(note, magazine))
+print(canConstruct(note, magazine2))
+print(canConstruct(note, magazine3))
