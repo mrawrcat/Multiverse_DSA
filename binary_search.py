9cba0f5605b8b65879fa67a22e53414621084a26
danwen <danwen@penguin>
2023-12-01 13:59:28 -0500

    added summary/explaination

--- binary_search.py
+++ binary_search.py
@@ -1,5 +1,7 @@
 '''
-Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+Given an array of integers nums which is sorted in ascending order,
+and an integer target, write a function to search target in nums. 
+If target exists, then return its index. Otherwise, return -1.
 
 You must write an algorithm with O(log n) runtime complexity.
 
@@ -9,4 +11,10 @@ Constraints:
 -104 < nums[i], target < 104
 All the integers in nums are unique.
 nums is sorted in ascending order.
+
+Summary/Explaination:
+
+Write a function that takes in an array of integers and a target.
+The function should return the index of the target from the array.
+If the target does not exist in the array return -1
 '''
