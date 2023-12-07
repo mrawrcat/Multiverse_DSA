f22a70e240429ead76588b102e6835670516ecb1
danwen <danwen@penguin>
2023-12-07 17:25:06 -0500

    added solution to problem

--- linkedLists.py
+++ linkedLists.py
@@ -24,4 +24,29 @@ Psuedocode planning
 To remove a node from the linked list we should first find our node by iterating through the
 linkedList to find the node we are deleting. When we find our node, we assign the next of
 the previous node to the next of the node after our current node.
+
+In the context of this problem we cannot delete this specific node. we are given the node
+but not the head of the list so we cannot iterate (search) through the list and do not know 
+the previous node to make the previous node's next the next node.
+We only know this node and the next (succeding) nodes .
+To "remove" the current node, we basically replace this node's next and value to the next
+node so that this node becomes the next node and points to the node after the next node.
+
+To do this we change this node's value and next to be the next node's value and next.
+If we want to remove the next node for garbage collection
+we set the next node's next to None/Null
 '''
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def deleteNode(self, node):
+        # set this node's value to next node's value
+        node.val = node.next.val
+        # set this node's next to next node's next
+        node.next = node.next.next
+        # if we want to garbage collect set next node's next to None
+        node.next.next 
---
+++ linkedLists_test.py
@@ -0,0 +1,5 @@
+from linkedLists import Solution
+
+linkedList = Solution()
+linkedList.deleteNode(4)
+print(linkedList)
